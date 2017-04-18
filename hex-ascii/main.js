f0f25a2ab3e6e14b1e9038a1d6a24aacd712be03
Quinton Erasmus <qcerasmus@gmail.com>
2017-04-18 21:31:03 +0200

    Managed to make main.js more compact.
    Signed-off-by: Quinton Erasmus <qcerasmus@gmail.com>

--- hex-ascii/main.js
+++ hex-ascii/main.js
@@ -1,27 +1,7 @@
-function ascii_value(c) {
-	// restrict input to a single character
-	// loop through all possible ASCII values
-	c = c[0]
-	for (var i = 0; i < 256; ++i) {
-		// convert i into a 2-digit hex string
-		var h = i.toString (16);
-		if (h.length == 1)
-			h = "0" + h;
-		// insert a % character into the string
-		h = "%" + h;
-		// determine the character represented by the escape code
-		h = unescape (h);
-		// if the characters match, we've found the ASCII value
-		if (h == c)
-			break;
-	}
-	return i;
-}
-
 function ascii_to_hexa(str, currentEditor) {
 	str = str.toString();
 	for (var n = 0, l = str.length; n < l; n++) {
-		var hex = Number(ascii_value(str[n])).toString(16);
+		var hex = Number(str.charCodeAt(n)).toString(16);
 		currentEditor.setSelectionsText([hex]);
 	}
 }

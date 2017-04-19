bb71c86a415b3758fe39b195786aaccbc948cb6e
Quinton Erasmus <qcerasmus@gmail.com>
2017-04-19 22:15:29 +0200

    added the actual command that should run.
    Signed-off-by: Quinton Erasmus <qcerasmus@gmail.com>

--- runner/main.js
+++ runner/main.js
@@ -12,8 +12,12 @@ NotepadqqApi.connect(function(api) {
 		var menuitemopenpython = window.addExtensionMenuItem(api.extensionId, "open file in terminal");
 		menuitemopenpython.on("triggered", function() {
 			if(window.currentEditor().fileName() != null && window.currentEditor().fileName() != "") {
-				var spawn = require('child_process').spawn;
-				var py = spawn('terminal', window.currentEditor().fileName());
+				
+				var filename = window.currentEditor().fileName().toString();
+				filename = filename.substring(7);
+				var command = 'gnome-terminal -x sh -c \'python "/home/qcerasmus/Desktop/test.py"\'; exec bash';
+				var terminal = require('child_process').spawn(command);
+				//gnome-terminal -x sh -c 'python "/home/qcerasmus/Desktop/test.py"; exec bash'
 			}
 		});
    	});
